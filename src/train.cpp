4350c6101b2d68b0b6e5b8dfa600d9a5c9ce9f2c
NastyaIlina07 <149273255+NastyaIlina07@users.noreply.github.com>
2025-05-18 19:33:49 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -3,6 +3,19 @@
 #include <iostream>
 Train::Train() : countOp(0), first(nullptr) {}
 
+Train::~Train() {
+  if (!first) return;
+
+  Car* current = first->next;
+  while (current != first) {
+    Car* nextCar = current->next;
+    delete current;
+    current = nextCar;
+  }
+  delete first;
+  first = nullptr;
+}
+
 void Train::addCar(bool light) {
   Car* newCar = new Car{light, nullptr, nullptr};
   if (!first) {
